6ae52016117dd15d20a3c96be073305835223914
Ryo Miyajima <sergeant.wizard@gmail.com>
2019-05-01 14:09:42 -0700

    Add adapter for AnkiDroid API

---
+++ app/src/main/java/com/example/ankienen/AnkiDroidAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.ankienen
+
+import com.ichi2.anki.api.AddContentApi
+
+fun defaultDeckId(api: AddContentApi): Long? {
+    for (pair in api.deckList) {
+        if (pair.value == "Default") {
+            return pair.key
+        }
+    }
+    return null
+}
+
+fun defaultModel(api: AddContentApi): Long? {
+    for (pair in api.modelList) {
+        if (pair.value == "Basic") {
+            return pair.key
+        }
+    }
+    return null
+}
+
+fun addCard(context: android.content.Context) {
+    val api = AddContentApi(context.applicationContext)
+    val deckId = defaultDeckId(api)
+    if (deckId == null) {
+        android.widget.Toast.makeText(context,"default deck not found", android.widget.Toast.LENGTH_SHORT).show()
+        return
+    }
+    val modelId = defaultModel(api)
+    if (modelId == null) {
+        android.widget.Toast.makeText(context,"model deck not found", android.widget.Toast.LENGTH_SHORT).show()
+        return
+    }
+    api.addNote(modelId, deckId, arrayOf("word", "meaning"), setOf())
+}
--- app/src/main/java/com/example/ankienen/ScrollingActivity.kt
+++ app/src/main/java/com/example/ankienen/ScrollingActivity.kt
@@ -3,9 +3,41 @@ package com.example.ankienen
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.Menu
 import android.view.MenuItem
+import android.view.ViewGroup
 import kotlinx.android.synthetic.main.activity_scrolling.*
+import kotlinx.android.synthetic.main.content_scrolling.*
+
+
+fun sendMeaning(view: android.view.View, entry: Entry) {
+    Snackbar.make(view, entry.meaning, Snackbar.LENGTH_LONG).setAction("Action", null).show()
+    // TODO: call addCard()
+}
+
+class MyAdapter(private val entries: Array<Entry>) :
+    RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+    class MyViewHolder(val buttonView: android.widget.Button) : RecyclerView.ViewHolder(buttonView)
+    override fun onCreateViewHolder(parent: ViewGroup,
+                                    viewType: Int): MyAdapter.MyViewHolder {
+        val buttonView = android.view.LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_list_component, parent, false) as android.widget.Button
+        buttonView.setOnClickListener {
+            view -> sendMeaning(view, view.tag as Entry)
+        }
+        return MyViewHolder(buttonView)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.buttonView.text = entries[position].meaning
+        holder.buttonView.tag = entries[position]
+    }
+
+    override fun getItemCount() = entries.size
+}
 
 class ScrollingActivity : AppCompatActivity() {
 
@@ -17,6 +49,9 @@ class ScrollingActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        val entries = arrayOf(Entry("word", "meaning"))
+        inflateButtons(entries)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -35,8 +70,9 @@ class ScrollingActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
-    fun onClick(view: android.view.View) {
-        Snackbar.make(view, "text was clicked", Snackbar.LENGTH_LONG)
-            .setAction("Action", null).show()
+
+    private fun inflateButtons(entries: Array<Entry>) {
+        button_list.adapter = MyAdapter(entries)
+        button_list.layoutManager = LinearLayoutManager(this)
     }
 }
